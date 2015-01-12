8c04de667c89a6ed8dc936d5c1da3ef09864e37d
mtleung <m.leung27@gmail.com>
2015-01-12 22:48:06 +0800

    Project init

---
+++ TranslateApp/src/com/iems5722/translateapp/MainActivity.java
@@ -0,0 +1,50 @@
+package com.iems5722.translateapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		// get references to layout objects
+
+		// add click listener to button to call translateText()
+
+	}
+	
+	// translate look up
+	private void translateText() {
+		// get user input
+
+		// try get word out of dictionary
+
+		// show some feedback to user: translated text, error message, dialog etc
+
+	}
+
+	
+	// Options menu - not needed for this app
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
